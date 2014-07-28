f002e5aea45d8fad0aee3d3f97ebdbf9d88f6273
Will Handley <wh260@cam.ac.uk>
2014-07-28 06:38:19 +0100

    Random utils is now in a completely commented state
    Currently has initialisation, deinitialisation, a random vector
    generator and a random coordinate generator

--- src/random_utils.f90
+++ src/random_utils.f90
@@ -1,4 +1,13 @@
-include 'mkl_vsl.f90'
+include 'mkl_vsl.f90'  ! Include the Intel Math Kernel Library - Vector Statistical Library
+                       ! This is a library used for generating random numbers optimally on intel cores
+
+!> Module containing utilities to generate random numbers
+!!
+!! For details on the Intel Math Kernel Library - Vector Statistical Library (MKL_VSL)
+!! consult the [excellent manual](
+!! https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/vslnotes/)
+!!
+!! @todo Save stream state for exact replication if necessary
 
 module random_module
 
@@ -7,20 +16,47 @@ module random_module
 
     implicit none
 
+    !> The stream state for vector random number generators
     type (vsl_stream_state) :: rng_stream
-    integer,parameter       :: basic_rng_type = VSL_BRNG_MT19937
-    integer                 :: seed
 
 
     contains
 
 
+    ! ===========================================================================================
+    
+
+    !> Initialise the random number generators
+    !! 
+    !! Takes an optional argument in case you want to set the seed. Otherwise it
+    !! just uses the system time. 
+    !!
+    !! The intel command vslnewstream initialises the module variable
+    !! rng_stream, which is then used throughout the code to generate random
+    !! numbers from. More can be found on streams [here](
+    !! https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/vslnotes/hh_goto.htm#7_3_2_Creating_and_Initializing_Random_Streams.htm)
+    !! We have opted to use a fast implementation of the Mersenne Twister as our
+    !! [basic random number generator.](
+    !! https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/vslnotes/hh_goto.htm#7_2_Basic_Generators.htm)
+    !!
+    !! @todo Error code processing
+    !!
+    !! @todo Write seed to log file
+    !!
+    !! @todo parallel stream initialisation
 
     subroutine initialise_random(seed_input)
         implicit none
+        !> Optional seed input.
+        !! If this isn't included, then the system time is used
         integer, optional, intent(in) :: seed_input
 
-        integer :: errcode
+        integer :: errcode ! Error code
+        integer :: seed    ! seed to be generated from system time
+
+        ! The choice of random number generator
+        ! In this case it is a fast mersenne twister
+        integer,parameter       :: basic_rng_type = VSL_BRNG_SFMT19937
 
 
         if (present(seed_input)) then
@@ -29,14 +65,53 @@ module random_module
         else
             ! Otherwise initialise it with the system time
             call system_clock(seed)
-            errcode=vslnewstream( rng_stream, brng,  seed )
+            errcode=vslnewstream( rng_stream, basic_rng_type,  seed )
         end if
-
 
     end subroutine initialise_random
 
+    ! ===========================================================================================
 
-    !>  Random direction vector generator
+
+    !>  Random unit hypercube coordinate
+    !!
+    !! Generate a randomly directed unit vector in the unit hypercube
+    !!
+    !! We use the 
+    !! [VSL_RNG_METHOD_UNIFORM_STD](
+    !! https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/vslnotes/hh_goto.htm#9_3_1_Uniform_VSL_RNG_METHOD_UNIFORM_STD.htm) 
+    !! intel method to generate Gaussian random numbers. Note that this can come
+    !! in an accurate form
+
+    subroutine random_coordinate(coordinates,nDims)
+        implicit none
+
+        !> The output nDims coordinate
+        double precision, intent(out), dimension(nDims) :: coordinates
+        !> Dimensions of the unit vector
+        integer,          intent(in)                    :: nDims
+
+        ! Method to generate random numbers 
+        ! (This can be upgraded to VSL_RNG_METHOD_UNIFORM_STD_ACCURATE)
+        integer,parameter       :: method=VSL_RNG_METHOD_UNIFORM_STD
+
+        double precision, parameter :: u_bound  = 0.0 ! generate random numbers between 0 and 1
+        double precision, parameter :: l_bound  = 1.0 ! 
+
+        integer :: errcode ! Error code
+
+
+        ! Generate nDims random numbers, stored in a vector 'coordinates'
+        ! (v=vector,d=double,rng,uniform)
+        errcode=vdrnguniform( method, rng_stream, ndims, coordinates, u_bound, l_bound)
+
+
+    end subroutine random_coordinate
+
+    ! ===========================================================================================
+
+
+    !>  Random direction vector
     !!
     !! Generate a randomly directed unit vector in nDims dimensional space
     !!
@@ -65,9 +140,9 @@ module random_module
         integer :: errcode ! Error code
 
 
-
         ! Generate nDims gaussian random numbers, stored in a vector nhat
         ! with mean=0 variance=1
+        ! (v=vector,d=double,rng,gaussian)
         errcode=vdrnggaussian( method, rng_stream, nDims, nhat, mean, sigma)
 
         ! normalise the vector
@@ -75,18 +150,17 @@ module random_module
 
         ! nhat is now a random direction
 
-
-
     end subroutine random_direction
 
+    ! ===========================================================================================
+    
 
-
-
+    !> De-initialise the random number generators
 
     subroutine deinitialise_random
 
         implicit none
-        integer :: errcode
+        integer :: errcode ! Error code
 
         errcode=vsldeletestream( rng_stream )
 
