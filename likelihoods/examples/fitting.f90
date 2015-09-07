4cf4f30ee7859fa998e6661bcbe133e325fa615e
Will Handley <wh260@cam.ac.uk>
2015-09-07 20:00:33 +0100

    Improved the fitting loglikelihood

--- likelihoods/examples/fitting.f90
+++ likelihoods/examples/fitting.f90
@@ -27,7 +27,6 @@ module loglikelihood_module
         double precision :: loglikelihood_temp
         integer :: i_stats
         integer :: i_int
-        integer,parameter :: n_int = 100
         double precision :: x,y
 
 
@@ -61,27 +60,14 @@ module loglikelihood_module
                 loglikelihood_temp = - log( sigmay(i_stats) ) - logTwoPi/2d0 - ((y-y0(i_stats))/sigmay(i_stats))**2/2d0 
 
             else
-                ! Calculate the minimum and maximum x's so that we can integrate
-                x_min_int = x0(i_stats)-5d0*sigmax(i_stats)
-                x_max_int = x0(i_stats)+5d0*sigmax(i_stats)
-
-                ! loglikelihood_temp denotes the likelihood associated with the point
-                loglikelihood_temp = logzero
-
-                ! do a simple integral by summing on equal intervals 
-                do i_int = 1,n_int
-                    x = x_min_int + (i_int-1d0)/(n_int-1d0) * (x_max_int-x_min_int)
-                    y = linear_interpolate(x,spline_data,0)
-                    call logincexp(loglikelihood_temp, - ((y-y0(i_stats))/sigmay(i_stats))**2/2d0 - ((x-x0(i_stats))/sigmax(i_stats))**2/2d0 )
-                end do
+                ! Integrate the spline
+                loglikelihood_temp = log_exp_int(x0(i_stats),y0(i_stats),sigmax(i_stats),sigmay(i_stats),x_min,x_max)
 
                 ! Normalise via the normalising constants, and the width of the
                 ! integral
-                ! note that the factor of (x_max_int-x_min_int) that should be here cancels out
-                ! with the prior
                 loglikelihood_temp = loglikelihood_temp   &
                     - log( sigmay(i_stats) ) - log( sigmax(i_stats) ) - logTwoPi &
-                    - log( n_int-1d0 )
+                    - log(x_max-x_min)
 
             end if
 
@@ -94,6 +80,129 @@ module loglikelihood_module
 
     end function loglikelihood
 
+
+
+
+
+    ! This function computes the logarithm of:
+    !
+    ! /xmax                                                
+    ! |     exp( - (x-x0)^2/2sx^2 - (y0-f(x))^2/2sy^2 ) dx 
+    ! /xmin                                                
+    !
+    ! where f(x) is a linear spline defined by spline_arr.
+    !
+    ! This is effectively a piecewise linear integral:
+    ! 
+    ! /xmax                                                
+    ! |     exp( - (x-x0)^2/2sx^2 - (y0-(m x + c))^2/2sy^2 ) dx 
+    ! /xmin                                                
+    !
+    ! where m and c change depending on which interval one is in.
+    !
+    ! Completing the square on the integrand yields:
+    !     1   /           \2     1        1   
+    ! - ----- | x - e s^2 |   - --- f  + --- e
+    !   2 s^2 \           /      2        2   
+    !
+    ! where 
+    !
+    ! s = (sx^-2 + m^2 sy^-2)^-(1/2)
+    ! e = x0/sx^2 + (y-c) m/ sy^2
+    ! f = x0^2 /sx^2 + (y-c)^2 / sy^2
+    !
+    ! The integral can then be computed with error functions, since
+    !
+    ! /x2
+    ! |      exp( (x-x0)^2/2s^2 ) dx = 
+    ! /x1
+    !
+    ! sqrt(pi/2) s ( erf( (x2-x0)/sqrt2 s) - erf( (x1-x0)/sqrt2 s) )
+    !
+    function log_exp_int(x0,y0,sx,sy,xmin,xmax)
+        use utils_module, only: logzero,logincexp
+        implicit none
+
+        double precision, intent(in) :: x0,y0,sx,sy,xmin,xmax
+        double precision :: log_exp_int
+
+        integer :: i,n
+        double precision :: m,c,x1,x2,y1,y2
+
+        double precision :: s,e,f
+
+        double precision,parameter :: logsqrtpiby2 = log(sqrt(atan(1d0)*2d0))
+
+        log_exp_int = logzero
+
+        n = size(spline_data,1)
+
+        do i=1,n-1
+
+            ! Here are some useful local variables
+            x1 = spline_data(i,1)
+            y1 = spline_data(i,2)
+            x2 = spline_data(i+1,1)
+            y2 = spline_data(i+1,2)
+
+            m = (y2-y1)/(x2-x1)
+            c =  y1 - m*x1
+
+            ! Check to see our integration range
+            if(x2<xmin) cycle
+            if(x1<xmin) x1 = xmin
+
+            if(x1>xmax) cycle
+            if(x2>xmax) x2 = xmax
+
+            ! Define the reduced variables
+            s  = (1/sx**2 + m**2/sy**2)**(-0.5)
+            e  = x0/sx**2 + (y0-c)*m/sy**2
+            f  = x0**2 /sx**2 + (y0-c)**2/sy**2
+
+
+            call logincexp(log_exp_int,&
+                logsqrtpiby2 + log(s) +&
+                logderf(&
+                (x1-e*s**2)/sqrt(2d0)/s ,&
+                (x2-e*s**2)/sqrt(2d0)/s  &
+                )&
+                - f/2 + e**2*s**2/2 &
+                )
+
+        end do
+
+    end function log_exp_int
+
+    function logderf(a,b)
+        use utils_module, only: logzero
+        implicit none
+        double precision,intent(in) :: a,b
+        double precision logderf
+
+        double precision :: erfa,erfb
+
+        erfb = erf(b)
+        erfa = erf(a)
+
+        if(erfb<=erfa) then
+            logderf = logzero
+        else
+            logderf = log(erfb-erfa)
+        end if
+
+    end function logderf
+
+    function erfapprox(x)
+        implicit none
+        double precision, intent(in) :: x
+        double precision erfapprox
+
+        double precision, parameter :: pi = atan(1d0)*4d0
+        double precision, parameter :: a = 8*(pi-3)/(3*pi*(4-pi))
+
+        erfapprox = sign(1d0,x)*sqrt(1-exp(-x**2*(4/pi + a*x**2)/(1+a*x**2)))
+    end function erfapprox
 
 
     subroutine setup_loglikelihood(settings,mpi_communicator)
@@ -153,6 +262,13 @@ module loglikelihood_module
             read(stats_unit,*) x0(i_stats), y0(i_stats), sigmax(i_stats), sigmay(i_stats) 
         end do
         close(stats_unit)
+
+        ! Read in the mins and maxs array
+        open(stats_unit,file='data/data_min_max.dat')
+        read(stats_unit,*) x_min
+        read(stats_unit,*) x_max
+        close(stats_unit)
+
 
 
     end subroutine setup_loglikelihood
