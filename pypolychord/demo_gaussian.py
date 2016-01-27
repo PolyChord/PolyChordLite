cd8c4d25c27edb8aa48c6755d429a3bfe0817a03
Will Handley <wh260@cam.ac.uk>
2016-01-27 23:34:45 +0000

    Added a readme for pypolychord

--- pypolychord/demo_gaussian.py
+++ pypolychord/demo_gaussian.py
@@ -3,11 +3,15 @@ from run import run_polychord
 
 def gaussian(theta, nderived, ndims):
     """ A simple gaussian with mean mu and variance sigma """
-    mu, sigma = 0.1, 0.5
-    loglike = 0
+    mu, sigma = 0.5, 0.1
+
+    loglike = - numpy.log(2*numpy.pi*sigma*sigma)/2*ndims
+
     for param in theta:
         loglike += -(param-mu)*(param-mu)/2/sigma/sigma
+
     phi = numpy.zeros(nderived)
+
     return loglike, phi
 
-run_polychord(gaussian, 20, file_root='gaussian')
+run_polychord(gaussian, 2, file_root='gaussian')
--- pypolychord/run.py
+++ pypolychord/run.py
@@ -4,10 +4,10 @@ import pypolychord
 def run_polychord(loglikelihood, nDims, 
                   nDerived=1,
                   nlive=50,
-                  num_repeats=20,
+                  num_repeats=None,
                   do_clustering=True,
-                  feedback=2,
-                  precision_criterion=0.0001,
+                  feedback=1,
+                  precision_criterion=0.001,
                   max_ndead=-1,
                   boost_posterior=0.0,
                   posteriors=True,
@@ -25,6 +25,9 @@ def run_polychord(loglikelihood, nDims,
                   grade_dims=[0],
                   grade_frac=[1.0]):
     """ Interface for running pypolychord. Default parameters may be adjusted as required """
+
+    if num_repeats is None:
+        num_repeats = nDims*5
 
     pypolychord.interfaces_module.simple_interface(loglikelihood, nlive, num_repeats, do_clustering, feedback, precision_criterion, max_ndead, boost_posterior, posteriors, equals, cluster_posteriors, write_resume, write_paramnames, read_resume, write_stats, write_live, write_dead, update_files, nDims, nDerived, base_dir, file_root, grade_dims, grade_frac)
 
