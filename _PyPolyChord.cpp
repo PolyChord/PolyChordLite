a4c4d48c38e05f27b111f98f9a29ba342cc623e7
Will Handley <wh260@cam.ac.uk>
2016-04-05 15:23:37 +0100

    Got the linear code working. Need to fix memory leak associated with INCREFs and DECREFs

--- _PyPolyChord.cpp
+++ _PyPolyChord.cpp
@@ -2,35 +2,18 @@
 #include "interfaces.h"
 #include <iostream>
 
-//double default_loglikelihood(double* theta, int nDims, double* phi, int nDerived)
-//{
-//    double logL = 0.0;
-//    for(int i=0;i<nDims;i++) logL -= theta[i]*theta[i];
-//    return logL;
-//}
-
-void default_prior(double* cube, double* theta, int nDims)
-{
-    for(int i=0;i<nDims;i++) theta[i] = cube[i]; 
-}
-
-
 /* Docstrings */
 static char module_docstring[] =
     "PyPolyChord: This module provides a Python interface to PolyChord.";
 static char run_docstring[] =
     "Runs PyPolyChord";
-static char set_loglikelihood_docstring[] =
-    "Sets the loglikelihood function";
 
 
 /* Available functions */
 static PyObject *run_PyPolyChord(PyObject *self, PyObject *args);
-static PyObject *set_loglikelihood_PyPolyChord(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
     {"run", run_PyPolyChord, METH_VARARGS, run_docstring},
-    {"set_loglikelihood", set_loglikelihood_PyPolyChord, METH_VARARGS, set_loglikelihood_docstring},
     {NULL, NULL, 0, NULL}
 };
 
@@ -44,48 +27,49 @@ PyMODINIT_FUNC init_PyPolyChord(void)
 }
 
 
-PyObject* makelist(double* array, int n) {
-    PyObject *list = PyList_New(n);
+/* Functions for converting from lists to arrays */
+void convert_list(double* array, PyObject* list) {
+    int n = PyList_Size(list);
     for (int i=0; i<n; i++) 
         PyList_SET_ITEM(list, i, PyFloat_FromDouble(array[i]));
-    return list;
 }
 
-/* Callback to the likelihood */
-static PyObject *python_loglikelihood = NULL;
-
-static PyObject *set_loglikelihood_PyPolyChord(PyObject *self, PyObject *args)
-{
-    PyObject *result = NULL;
-    PyObject *temp;
-
-    if (PyArg_ParseTuple(args, "O", &temp)) {
-        if (!PyCallable_Check(temp)) {
-            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
-            return NULL;
-        }
-        Py_XINCREF(temp);                  /* Add a reference to new callback */
-        Py_XDECREF(python_loglikelihood);  /* Dispose of previous callback */
-        python_loglikelihood = temp;       /* Remember new callback */
-
-        /* Boilerplate to return "None" */
-        Py_INCREF(Py_None);
-        result = Py_None;
+void convert_list(PyObject* list, double* array) {
+    PyObject* temp;
+    int n = PyList_Size(list);
+    for (int i=0; i<n; i++) 
+    {
+        temp = PyList_GET_ITEM(list, i);
+        array[i] = PyFloat_AsDouble(temp);
     }
-    return result;
 }
 
+/* Callback to the likelihood and prior */
+static PyObject *python_loglikelihood = NULL;
 
 double loglikelihood(double* theta, int nDims, double* phi, int nDerived)
 {
-    PyObject* list_theta = makelist(theta,nDims);
-    PyObject* list_phi = makelist(phi,nDerived);
+    PyObject* list_theta = PyList_New(nDims);
+    convert_list(theta,list_theta);
+
+    PyObject* list_phi = PyList_New(nDerived);
+    convert_list(phi,list_phi);
+
     PyObject* pylogL = PyObject_CallFunctionObjArgs(python_loglikelihood,list_theta,list_phi,NULL);
-    Py_DECREF(list_theta);
-    Py_DECREF(list_phi);
-    double logL = PyLong_AsDouble(pylogL);
-    Py_DECREF(pylogL);
+
+    double logL = PyFloat_AsDouble(pylogL);
     return logL;
+}
+
+static PyObject *python_prior = NULL;
+
+void prior(double* cube, double* theta, int nDims)
+{
+    PyObject* list_cube = PyList_New(nDims);
+    convert_list(cube,list_cube);
+
+    PyObject* list_theta = PyObject_CallFunctionObjArgs(python_prior,list_cube,NULL);
+    convert_list(list_theta,theta);
 }
 
 /* Function to run PyPolyChord */
@@ -113,15 +97,13 @@ static PyObject *run_PyPolyChord(PyObject *self, PyObject *args)
     char* base_dir;
     char* file_root;
 
-
     /* Parse the input tuple */
-    if (!PyArg_ParseTuple(args, "iiiiiididiiiiiiiiiiss", &nDims, &nDerived, &nlive, &num_repeats, &do_clustering, &feedback, &precision_criterion, &max_ndead, &boost_posterior, &posteriors, &equals, &cluster_posteriors, &write_resume, &write_paramnames, &read_resume, &write_stats, &write_live, &write_dead, &update_files, &base_dir, &file_root))
+    if (!PyArg_ParseTuple(args, "OOiiiiiididiiiiiiiiiiss", &python_loglikelihood, &python_prior, &nDims, &nDerived, &nlive, &num_repeats, &do_clustering, &feedback, &precision_criterion, &max_ndead, &boost_posterior, &posteriors, &equals, &cluster_posteriors, &write_resume, &write_paramnames, &read_resume, &write_stats, &write_live, &write_dead, &update_files, &base_dir, &file_root))
         return NULL;
-
 
     polychord_c_interface( 
             loglikelihood, 
-            default_prior, 
+            prior, 
             nlive, 
             num_repeats,
             do_clustering,
@@ -144,10 +126,8 @@ static PyObject *run_PyPolyChord(PyObject *self, PyObject *args)
             base_dir,
             file_root);
 
-
     /* Build the output tuple */
     PyObject *ret = Py_None;
-    Py_INCREF(Py_None);
     return ret;
 }
 
--- run_PyPolyChord.py
+++ run_PyPolyChord.py
@@ -24,18 +24,23 @@ update_files = nlive
 base_dir = 'chains'
 file_root = 'PyPolyChord_test'
 
+from numpy import pi,log
+
 def gaussian(theta,phi):
-    from numpy import pi,log
     sigma = 0.1
     nDims = len(theta)
 
-    logL = -log(2*pi)*nDims/2.0
+    logL = -log(2*pi*sigma*sigma)*nDims/2.0
 
     for t in theta:
         logL += -(t-0.5)*(t-0.5)/2/sigma/sigma
 
-    return long(logL)
+    return logL
 
-_PyPolyChord.set_loglikelihood(gaussian)
+def prior(cube):
+    theta = cube
+    return theta
 
-_PyPolyChord.run(nDims, nDerived, nlive, num_repeats, do_clustering, feedback, precision_criterion, max_ndead, boost_posterior, posteriors, equals, cluster_posteriors, write_resume, write_paramnames, read_resume, write_stats, write_live, write_dead, update_files, base_dir, file_root)
+#_PyPolyChord.set_loglikelihood(gaussian)
+
+_PyPolyChord.run(gaussian, prior, nDims, nDerived, nlive, num_repeats, do_clustering, feedback, precision_criterion, max_ndead, boost_posterior, posteriors, equals, cluster_posteriors, write_resume, write_paramnames, read_resume, write_stats, write_live, write_dead, update_files, base_dir, file_root)
