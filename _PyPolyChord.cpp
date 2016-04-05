437667119b55261386f4aa7c2efb33280776909f
Will Handley <wh260@cam.ac.uk>
2016-04-05 14:47:51 +0100

    Got something working with function

--- _PyPolyChord.cpp
+++ _PyPolyChord.cpp
@@ -2,12 +2,12 @@
 #include "interfaces.h"
 #include <iostream>
 
-double default_loglikelihood(double* theta, int nDims, double* phi, int nDerived)
-{
-    double logL = 0.0;
-    for(int i=0;i<nDims;i++) logL -= theta[i]*theta[i];
-    return logL;
-}
+//double default_loglikelihood(double* theta, int nDims, double* phi, int nDerived)
+//{
+//    double logL = 0.0;
+//    for(int i=0;i<nDims;i++) logL -= theta[i]*theta[i];
+//    return logL;
+//}
 
 void default_prior(double* cube, double* theta, int nDims)
 {
@@ -20,15 +20,20 @@ static char module_docstring[] =
     "PyPolyChord: This module provides a Python interface to PolyChord.";
 static char run_docstring[] =
     "Runs PyPolyChord";
+static char set_loglikelihood_docstring[] =
+    "Sets the loglikelihood function";
 
 
 /* Available functions */
 static PyObject *run_PyPolyChord(PyObject *self, PyObject *args);
+static PyObject *set_loglikelihood_PyPolyChord(PyObject *self, PyObject *args);
 
 static PyMethodDef module_methods[] = {
     {"run", run_PyPolyChord, METH_VARARGS, run_docstring},
+    {"set_loglikelihood", set_loglikelihood_PyPolyChord, METH_VARARGS, set_loglikelihood_docstring},
     {NULL, NULL, 0, NULL}
 };
+
 
 /* Initialize the module */
 PyMODINIT_FUNC init_PyPolyChord(void)
@@ -36,6 +41,51 @@ PyMODINIT_FUNC init_PyPolyChord(void)
     PyObject *m = Py_InitModule3("_PyPolyChord", module_methods, module_docstring);
     if (m == NULL)
         return;
+}
+
+
+PyObject* makelist(double* array, int n) {
+    PyObject *list = PyList_New(n);
+    for (int i=0; i<n; i++) 
+        PyList_SET_ITEM(list, i, PyFloat_FromDouble(array[i]));
+    return list;
+}
+
+/* Callback to the likelihood */
+static PyObject *python_loglikelihood = NULL;
+
+static PyObject *set_loglikelihood_PyPolyChord(PyObject *self, PyObject *args)
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O", &temp)) {
+        if (!PyCallable_Check(temp)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);                  /* Add a reference to new callback */
+        Py_XDECREF(python_loglikelihood);  /* Dispose of previous callback */
+        python_loglikelihood = temp;       /* Remember new callback */
+
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+
+double loglikelihood(double* theta, int nDims, double* phi, int nDerived)
+{
+    PyObject* list_theta = makelist(theta,nDims);
+    PyObject* list_phi = makelist(phi,nDerived);
+    PyObject* pylogL = PyObject_CallFunctionObjArgs(python_loglikelihood,list_theta,list_phi,NULL);
+    Py_DECREF(list_theta);
+    Py_DECREF(list_phi);
+    double logL = PyLong_AsDouble(pylogL);
+    Py_DECREF(pylogL);
+    return logL;
 }
 
 /* Function to run PyPolyChord */
@@ -70,7 +120,7 @@ static PyObject *run_PyPolyChord(PyObject *self, PyObject *args)
 
 
     polychord_c_interface( 
-            default_loglikelihood, 
+            loglikelihood, 
             default_prior, 
             nlive, 
             num_repeats,
--- run_PyPolyChord.py
+++ run_PyPolyChord.py
@@ -3,7 +3,7 @@
 import _PyPolyChord
 
 nDims = 5
-nDerived = 1
+nDerived = 2
 nlive = 500
 num_repeats = nDims*5
 do_clustering = False
@@ -23,5 +23,19 @@ write_dead = False
 update_files = nlive
 base_dir = 'chains'
 file_root = 'PyPolyChord_test'
+
+def gaussian(theta,phi):
+    from numpy import pi,log
+    sigma = 0.1
+    nDims = len(theta)
+
+    logL = -log(2*pi)*nDims/2.0
+
+    for t in theta:
+        logL += -(t-0.5)*(t-0.5)/2/sigma/sigma
+
+    return long(logL)
+
+_PyPolyChord.set_loglikelihood(gaussian)
 
 _PyPolyChord.run(nDims, nDerived, nlive, num_repeats, do_clustering, feedback, precision_criterion, max_ndead, boost_posterior, posteriors, equals, cluster_posteriors, write_resume, write_paramnames, read_resume, write_stats, write_live, write_dead, update_files, base_dir, file_root)
