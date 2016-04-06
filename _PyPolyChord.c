026bc5017bda3569df0d3385abb3f4e38d63ba9f
Will Handley <wh260@cam.ac.uk>
2016-04-06 01:12:03 +0100

    C linear works

--- _PyPolyChord.c
+++ _PyPolyChord.c
@@ -4,6 +4,7 @@
 
 extern void polychord_c_interface(double (*)(double*,int,double*,int), void (*)(double*,double*,int), int, int, bool, int, double, int, double, bool, bool, bool, bool, bool, bool, bool, bool, bool, int, int, int, char*, char*); 
 
+
 /* Docstrings */
 static char module_docstring[] =
     "PyPolyChord: This module provides a Python interface to PolyChord.";
@@ -89,35 +90,39 @@ void prior(double* cube, double* theta, int nDims)
     Py_DECREF(list_theta);
 }
 
+
 /* Function to run PyPolyChord */
 static PyObject *run_PyPolyChord(PyObject *self, PyObject *args)
 {
     /* Inputs to PolyChord in the order that they are passed to python */
-    int nDims;
-    int nDerived;
-    int nlive;
-    int num_repeats;
-    bool do_clustering;
-    bool feedback;
-    double precision_criterion;
-    int max_ndead;
-    double boost_posterior;
-    bool posteriors;
-    bool equals;
-    bool cluster_posteriors;
-    bool write_resume;
-    bool write_paramnames;
-    bool read_resume;
-    bool write_stats;
-    bool write_live;
-    bool write_dead;
-    int update_files;
-    char* base_dir;
-    char* file_root;
+    int i=0;
+    python_loglikelihood = PyTuple_GetItem(args,i++);
+    python_prior          = PyTuple_GetItem(args,i++);
+
+    int nDims                  = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++));
+    int nDerived               = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++)); 
+    int nlive                  = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++));  
+    int num_repeats            = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++));
+    bool do_clustering         = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    int feedback               = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++));
+    double precision_criterion = (double) PyFloat_AsDouble( PyTuple_GetItem(args,i++));
+    int max_ndead              = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++));
+    double boost_posterior     = (double) PyFloat_AsDouble( PyTuple_GetItem(args,i++));
+    bool posteriors            = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool equals                = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool cluster_posteriors    = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool write_resume          = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool write_paramnames      = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool read_resume           = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool write_stats           = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool write_live            = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    bool write_dead            = (bool)   PyObject_IsTrue(  PyTuple_GetItem(args,i++));
+    int update_files           = (int)    PyInt_AsLong(     PyTuple_GetItem(args,i++)); 
+    char* base_dir             =          PyString_AsString(PyTuple_GetItem(args,i++));
+    char* file_root            =          PyString_AsString(PyTuple_GetItem(args,i++));
 
     /* Parse the input tuple */
-    if (!PyArg_ParseTuple(args, "OOiiiiiididiiiiiiiiiiss", &python_loglikelihood, &python_prior, &nDims, &nDerived, &nlive, &num_repeats, &do_clustering, &feedback, &precision_criterion, &max_ndead, &boost_posterior, &posteriors, &equals, &cluster_posteriors, &write_resume, &write_paramnames, &read_resume, &write_stats, &write_live, &write_dead, &update_files, &base_dir, &file_root))
-        return NULL;
+
 
     /* Run PolyChord */
     polychord_c_interface( 
