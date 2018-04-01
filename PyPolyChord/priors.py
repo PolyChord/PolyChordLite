3104315d50e56c90b44c3daba5a2b6025782e0fd
Will Handley <wh260@cam.ac.uk>
2018-04-01 19:53:38 +0100

    Updated the priors file

--- PyPolyChord/priors.py
+++ PyPolyChord/priors.py
@@ -25,12 +25,22 @@ class LogUniformPrior(UniformPrior):
         return self.a * (self.b/self.a) ** x
 
 
-class SortedUniformPrior(UniformPrior):
+def forced_indentifiability_transform(x):
+    N = len(x)
+    t = numpy.zeros(N)
+    t[N-1] = x[N-1]**(1./N)
+    for n in range(N-2,-1,-1):
+        t[n] = x[n]**(1./(n+1)) * t[n+1]
+    return t
 
+
+class SortedUniformPrior(UniformPrior):
     def __call__(self, x):
-        N = len(x)
-        t = numpy.zeros(N)
-        t[N-1] = x[N-1]**(1./N)
-        for n in range(N-2,-1,-1):
-            t[n] = x[n]**(1./(n+1)) * t[n+1]
-        return UniformPrior.__call__(self, t)
+        t = forced_indentifiability_transform(x)
+        return super(SortedUniformPrior, self).__call__(t)
+
+
+class LogSortedUniformPrior(LogUniformPrior):
+    def __call__(self, x):
+        t = forced_indentifiability_transform(x)
+        return super(LogSortedUniformPrior, self).__call__(t)
