5aa3acb22c13ad5c7b6b6d3706e8791be5e24d9e
polychordlondon <polychord@USER-PC.home>
2018-11-15 09:50:43 +0000

    save 2

--- likelihoods/CC_ini/CC_likelihood.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CC_likelihood_HPP
-#define CC_likelihood_HPP
-
-double loglikelihood (double theta[], int nDims, double phi[], int nDerived);
-void prior (double cube[], double theta[], int nDims);
-void dumper(int,int,int,double*,double*,double*,double, double);
-void setup_loglikelihood();
-
-#endif
