08aa08eb6f608689a64d5b0c2a52aa59256584a2
Will Handley <wh260@cam.ac.uk>
2019-11-21 15:31:43 +0000

    changed import line for MPI

--- run_pypolychord.py
+++ run_pypolychord.py
@@ -3,7 +3,7 @@ import pypolychord
 from pypolychord.settings import PolyChordSettings
 from pypolychord.priors import UniformPrior
 try:
-    import mpi4py
+    from mpi4py import MPI
 except ImportError:
     pass
 
