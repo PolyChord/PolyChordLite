4cc9667ff07909a0547a9ce6428cf772ee396c99
Aleksandr Petrosyan <a-p-petrosyan@Yandex.ru>
2020-06-05 02:01:27 +0400

    Added PEP8 compliant blank space.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def get_version(short=False):
             if 'version' in line:
                 return line[44:50]
 
+
 pypolychord_module = Extension(
         name='_pypolychord',
         library_dirs=[os.path.join(os.getcwd(), 'lib')],
