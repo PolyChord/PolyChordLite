6dc3d85b00617d1e1387827882ab53d19e3a80a0
T. K. Stenczel <tks32@cam.ac.uk>
2021-07-26 15:15:18 +0100

    directly build the python .so file

--- setup_cmake_template.py
+++ setup_cmake_template.py
@@ -1,46 +1,40 @@
+"""
+Template for setup.py that should be populated by CMake with:
+configure_file(${SETUP_PY_IN} ${SETUP_PY})
+
+Used template variables:
+    - "target_python_so" : place where _pypolychord.ARCH.so is built (NOT install path)
+    - "PACKAGE_VERSION" : version of the package
+    - "CMAKE_CURRENT_SOURCE_DIR": root directory, where a standard setup.py would be placed
+"""
+
 import os
 import shutil
 import sys
 from distutils.core import setup
-from pathlib import Path
 
 from setuptools import Extension
 from setuptools.command.build_ext import build_ext
 
 
 class MyBuildExtension(build_ext):
+    """This class 'builds' the extension module, by
+    copying it from the place where CMake placed it.
+    """
     def build_extension(self, ext):
-        print("MY current working directory:", os.getcwd())
-        print("MY args:", sys.argv)
 
         # _pypolychord.ARCH.so
-        if os.path.exists("${target_python_dir}/_pypolychord${python_so_suffix}"):
-            print("CP: ${target_python_dir}/_pypolychord${python_so_suffix} ----> ", self.get_ext_fullpath(ext.name))
-            shutil.copyfile("${target_python_dir}/_pypolychord${python_so_suffix}", self.get_ext_fullpath(ext.name))
-        else:
-            print("NOT FOUND: ${target_python_dir}/_pypolychord${python_so_suffix}")
-
-        # lib dir
-        if not os.path.isdir("${target_lib_dir}/pypolychord/lib/"):
-            print("CREATED: ${target_lib_dir}/pypolychord/lib/")
-            Path("${target_lib_dir}/pypolychord/lib/").mkdir(parents=True)
-
-        # libchord.so
-        if os.path.exists("${target_lib_dir}/libchord.so"):
-            print("CP ${target_lib_dir}/libchord.so -->",
-                  os.path.join(os.path.dirname(self.get_ext_fullpath(ext.name)), "pypolychord", "lib", "libchord.so"))
-            shutil.copyfile("${target_lib_dir}/libchord.so",
-                            os.path.join(os.path.dirname(self.get_ext_fullpath(ext.name)), "pypolychord", "lib", "libchord.so"))
-
-        else:
-            print("NOT Found: ${target_lib_dir}/libchord.so")
+        if os.path.exists("${target_python_so}"):
+            shutil.copyfile("${target_python_so}", self.get_ext_fullpath(ext.name))
+        elif sys.argv[2] == "install":
+            # let's warn here, though this should not happen with the current CMake setup
+            print("NOT FOUND: ${target_python_so}\nYour installation may be incomplete.")
 
 
 setup(name='pypolychord',
       version='${PACKAGE_VERSION}',
       package_dir={'pypolychord': '${CMAKE_CURRENT_SOURCE_DIR}/pypolychord'},
       packages=['pypolychord'],
-      package_data={'pypolychord': ["${target_lib_dir}/libchord.so"]},
       cmdclass={'build_ext': MyBuildExtension},
       ext_modules=[Extension('_pypolychord', [])],
       )
