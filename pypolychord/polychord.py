3f21a7bf9be9e6ab5685fa37a2823dae28d0c24c
Ormorod <Adam.Ormondroyd@gmail.com>
2022-07-27 18:55:10 +0100

    added kwargs to run_pypolychord() docstring

--- pypolychord/polychord.py
+++ pypolychord/polychord.py
@@ -55,6 +55,13 @@ def run_pypolychord(loglikelihood, nDims, nDerived,
         -------
         logL: float
             log-likelihood
+        TODO: what about []?
+
+    nDims: int
+        Dimensionality of the model, i.e. the number of physical parameters.
+
+    nDerived: int
+        The number of derived parameters (can be 0).
 
     settings: settings.Settings
         Object containing polychord settings
@@ -95,6 +102,160 @@ def run_pypolychord(loglikelihood, nDims, nDerived,
             The current log-evidence estimate
         logZerr: float
             The current log-evidence error estimate
+        
+    Keyword arguments
+    -----------------
+    nlive: int
+        (Default: nDims*25)
+        The number of live points.
+        Increasing nlive increases the accuracy of posteriors and evidences,
+        and proportionally increases runtime ~ O(nlive).
+
+    num_repeats : int
+        (Default: nDims*5)
+        The number of slice slice-sampling steps to generate a new point.
+        Increasing num_repeats increases the reliability of the algorithm.
+        Typically
+        * for reliable evidences need num_repeats ~ O(5*nDims).
+        * for reliable posteriors need num_repeats ~ O(nDims)
+
+    nprior : int
+        (Default: nlive)
+        The number of prior samples to draw before starting compression.
+
+    nfail : int
+        (Default: nlive)
+        The number of failed spawns before stopping nested sampling.
+
+    do_clustering : boolean
+        (Default: True)
+        Whether or not to use clustering at run time.
+
+    feedback : {0,1,2,3}
+        (Default: 1)
+        How much command line feedback to give
+
+    precision_criterion : float
+        (Default: 0.001)
+        Termination criterion. Nested sampling terminates when the evidence
+        contained in the live points is precision_criterion fraction of the
+        total evidence.
+
+    logzero : float
+        (Default: -1e30)
+        The loglikelihood value at which PolyChord considers points
+        'unphysical', and excludes them at the prior level.
+
+    max_ndead : int
+        (Default: -1)
+        Alternative termination criterion. Stop after max_ndead iterations.
+        Set negative to ignore (default).
+
+    boost_posterior : float
+        (Default: 0.0)
+        Increase the number of posterior samples produced.  This can be set
+        arbitrarily high, but you won't be able to boost by more than
+        num_repeats
+        Warning: in high dimensions PolyChord produces _a lot_ of posterior
+        samples. You probably don't need to change this
+
+    posteriors : boolean
+        (Default: True)
+        Produce (weighted) posterior samples. Stored in <root>.txt.
+
+    equals : boolean
+        (Default: True)
+        Produce (equally weighted) posterior samples. Stored in
+        <root>_equal_weights.txt
+
+    cluster_posteriors : boolean
+        (Default: True)
+        Produce posterior files for each cluster?
+        Does nothing if do_clustering=False.
+
+    write_resume : boolean
+        (Default: True)
+        Create a resume file.
+
+    read_resume : boolean
+        (Default: True)
+        Read from resume file.
+
+    write_stats : boolean
+        (Default: True)
+        Write an evidence statistics file.
+
+    write_live : boolean
+        (Default: True)
+        Write a live points file.
+
+    write_dead : boolean
+        (Default: True)
+        Write a dead points file.
+
+    write_prior : boolean
+        (Default: True)
+        Write a prior points file.
+
+    maximise : boolean
+        (Default: False)
+        Perform maximisation at the end of the run to find the maximum
+        likelihood point and value
+
+    compression_factor : double
+        (Default: exp(-1))
+        How often to update the files and do clustering
+
+    synchronous : boolean
+        (Default: True)
+        Parallelise with synchronous workers, rather than asynchronous ones.
+        This can be set to False if the likelihood speed is known to be
+        approximately constant across the parameter space. Synchronous
+        parallelisation is less effective than asynchronous by a factor ~O(1)
+        for large parallelisation.
+
+    base_dir : string
+        (Default: 'chains')
+        Where to store output files.
+
+    file_root : string
+        (Default: 'test')
+        Root name of the files produced.
+
+    grade_frac : List[float]
+        (Default: [1])
+        The amount of time to spend in each speed. 
+        If any of grade_frac are <= 1, then polychord will time each sub-speed,
+        and then choose num_repeats for the number of slowest repeats, and
+        spend the proportion of time indicated by grade_frac. Otherwise this
+        indicates the number of repeats to spend in each speed.
+
+    grade_dims : List[int]
+        (Default: nDims)
+        The number of parameters within each speed.
+
+    nlives : dict {double:int}
+        (Default: {})
+        Variable number of live points option. This dictionary is a mapping
+        between loglike contours and nlive.
+        You should still set nlive to be a sensible number, as this indicates
+        how often to update the clustering, and to define the default value.
+    seed : positive int
+        (Default: system time in milliseconds)
+        Choose the seed to seed the random number generator.
+        Note **Positive seeds only**
+        a negative seed indicates that you should use the system time in
+        milliseconds
+    cube_samples: array-like
+        (Default: None)
+        samples from the unit hypercube to start nested sampling from. This is
+        useful if the prior is hard to rejection sample directly from the unit
+        hypercube (e.g. if there is a large amount of excluded parameter
+        space), but can be done more efficiently manually. A concrete example
+        could be include sorted uniform parameters by requiring 0 < x1 < x2 <
+        x3 < 1, if one did not want to use SortedUniformPrior.
+        Only available in Python interface.
+        shape (:, nDims)
 
     Returns
     -------
